933731a5b5f274d061f14b0e6793b4e60f06680b
habtom gebre <hatomg127@gmail.com>
2020-03-05 15:27:40 -0600

    about me updated

--- app/src/main/java/com/sng/homework6/ui/aboutMe/AboutMeFragment.kt
+++ app/src/main/java/com/sng/homework6/ui/aboutMe/AboutMeFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.sng.homework6.R
+import kotlinx.android.synthetic.main.fragment_about_me.*
 
 class AboutMeFragment : Fragment() {
 
@@ -19,13 +21,18 @@ class AboutMeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        aboutMeViewModel =
-                ViewModelProviders.of(this).get(AboutMeViewModel::class.java)
+//        aboutMeViewModel =
+//                ViewModelProviders.of(this).get(AboutMeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_about_me, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        aboutMeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val title: TextView = root.findViewById(R.id.titleId)
+        var image = root.findViewById<ImageView>(R.id.imageView2)
+        var goodId = root.findViewById<TextView>(R.id.goodId)
+        var badId = root.findViewById<TextView>(R.id.badId)
+
+//        aboutMeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
         return root
     }
 }
